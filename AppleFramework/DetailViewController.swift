acfad258325eea2ddd03a499193086070eb1ed5f
torinunna <torinunna9588@gmail.com>
2022-07-16 01:42:30 +0900

    Combine DetailViewController

--- AppleFramework/DetailViewController.swift
+++ AppleFramework/DetailViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SafariServices
+import Combine
 
 class DetailViewController: UIViewController {
     
@@ -14,29 +15,44 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
-    var framework: AppleFramework = AppleFramework(name: "Unknown", imageName: "", urlString: "", description: "")
+    @Published var framework: AppleFramework = AppleFramework(name: "Unknown", imageName: "", urlString: "", description: "")
+    var subscriptions = Set<AnyCancellable>()
+    
+    let buttonPressed = PassthroughSubject<AppleFramework, Never>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateUI()
+        
+        bind()
 
     }
     
-    func updateUI() {
-        imageView.image = UIImage(named: framework.imageName)
-        titleLabel.text = framework.name
-        descriptionLabel.text = framework.description
-    
+    private func bind() {
+        
+//        Input
+        buttonPressed
+            .receive(on: RunLoop.main)
+            .compactMap { URL(string: $0.urlString) }
+            .sink { [unowned self] url in
+                let safari = SFSafariViewController(url: url)
+                present(safari, animated: true)
+            }.store(in: &subscriptions)
+        
+//        Output
+        $framework
+            .receive(on: RunLoop.main)
+            .sink { [unowned self] framework in
+                self.imageView.image = UIImage(named: framework.imageName)
+                self.titleLabel.text = framework.name
+                self.descriptionLabel.text = framework.description
+            }.store(in: &subscriptions)
     }
+
     
     @IBAction func learnMorePressed(_ sender: Any) {
         
-        guard let url = URL(string: framework.urlString) else {
-            return
-        }
+        buttonPressed.send(framework)
         
-        let safari = SFSafariViewController(url: url)
-        present(safari, animated: true)
     }
     
 }
